4f8770089ccafdf9ddc8c9131e46c3f741b3c2b1
jenous11 <jenousdongol11@gmail.com>
2026-03-06 21:23:10 +0545

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<h1>Hello Welcome to my Laravel Page</h1>
+<h2>
+Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Asperiores aut officia reiciendis sed fugit in exercitationem vel nostrum alias error tempora modi,
+obcaecati nisi recusandae? Maiores possimus vitae ducimus in?
+</h2>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,1 @@
+<h1>home page no blade </h1>
