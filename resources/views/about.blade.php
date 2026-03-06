f89d13fda0929090f1e1483fe2e5d2aef3da8d45
jenous11 <jenousdongol11@gmail.com>
2026-03-06 22:09:36 +0545

    learned about dd function

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,5 @@
-<h1>Hello Welcome to my Laravel Page</h1>
+<h1>Hello Welcome to my Laravel Page </h1>
 <h2>
-Lorem ipsum dolor sit amet consectetur adipisicing elit.
-Asperiores aut officia reiciendis sed fugit in exercitationem vel nostrum alias error tempora modi,
-obcaecati nisi recusandae? Maiores possimus vitae ducimus in?
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur ipsam necessitatibus numquam
+  dolore voluptates molestias tempore suscipit possimus nemo, pariatur assumenda sit? Magnam itaque quaerat vitae modi ullam tenetur nulla?
 </h2>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',function(){
+  $person=[
+    "name"=>"Jenous",
+    "email"=>"jenousdongol11@gmail.com"
+  ];
+  // dump($person);
+  dd($person);
   return view('welcome');
 });
 
-Route::get('/about',function(){
-  return view('idk');
-});
